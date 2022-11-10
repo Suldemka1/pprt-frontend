cd365c64cc1ff530583781814d287effc5d70511
Suldemka1 <suldemka1@gmail.com>
2022-11-10 15:10:11 +0700

    fix

--- pages/activity/npa.js
+++ pages/activity/npa.js
@@ -9,15 +9,17 @@ import StandartLayout from '../../layouts/StandartLayout';
 import DocumentLink from '../../components/Document/DocumentLink';
 
 export const getServerSideProps = async () => {
-    const res = await fetch(`${process.env.APIpath}/api/list-of-npa?populate=*`)
+    const res = await fetch(`${process.env.APIpath}/api/npas?populate=*`)
     const data = await res.json()
 
     return {
-        props: data.data
+        props: {
+            documents: data.data
+        }
     }
 }
 
-const Npa = (props) => {
+const Npa = ({ documents }) => {
     const router = useRouter()
     return (
         <StandartLayout>
@@ -27,17 +29,12 @@ const Npa = (props) => {
             <PageName title="Перечень законов и иных нормативно-правовых актов, определяющих полномочия, задачи, функции государственного органа" />
             <ActivityPageLayout>
                 <div className='flex flex-col gap-2'>
-                    <DocumentLink filename="Постановление Правительства Республики Тыва от 31 октября 2007 года № 976 «Об  утверждении Положения о Полномочном представительстве Республики Тыва в г. Москве»;"
-                        url="https://api.pprt.rtyva.ru/uploads/Postanovlenie_Pravitelstva_Respubliki_Tyva_Ob_utverzhdenii_Polozheniya_o_Polnomochnom_predstavitelstv_Moskve_d197824cd1.docx" />
-                    <DocumentLink filename="Постановление Правительства Республики Тыва от 29 декабря 2007 года № 1104 «О порядке
-                        взаимодействия органов исполнительной власти Республики Тыва с Полномочным
-                        представительством Республики Тыва в г. Москве»;"
-                        url="https://api.pprt.rtyva.ru/uploads/906700169_1_e7fa0d3329.pdf" />
-                    <DocumentLink filename="Постановление Правительства Республики Тыва от 26 мая 2014 года № 218 «Об утверждении
-                        Положения о порядке формирования, защиты предложений и бюджетных заявок на участие
-                        Республики Тыва в государственных программах Российской Федерации, федеральных целевых
-                        программах и федеральной адресной инвестиционной программе»."
-                        url="https://api.pprt.rtyva.ru/uploads/Postanovlenie_Pravitelstva_Respubliki_Tyva_ot_26_maya_2014_goda_218_6e1cfff7ed.docx" />
+                    {
+                        documents.map((item) =>
+                            <DocumentLink filename={item.document[0].name} url={`${item.document[0].url}`} />
+                        )
+                    }
+
                 </div>
             </ActivityPageLayout>
         </StandartLayout>
